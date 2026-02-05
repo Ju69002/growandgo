86e47a5cc2a0601728dfc0d01199edd286060d9e
Julien Secchi <julien.secchi5@gmail.com>
2026-02-05 14:01:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9807573620-aa64b",
+  "appId": "1:500181405818:web:8f283eff9c01a19c602bc3",
+  "apiKey": "AIzaSyA_rXr1gt4JkH16rkE_2K2VilFPPZusmng",
+  "authDomain": "studio-9807573620-aa64b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "500181405818"
+};
